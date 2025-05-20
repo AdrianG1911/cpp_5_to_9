4489294ca0214d952b8ae6a980fc61df5a199237
Adrian Gutierrez Valero <adrgutie@c3r3s1.42tokyo.jp>
2025-05-20 20:57:30 +0900

    good progress made on cpp_09

--- cpp_09/ex02/jf.cpp
+++ cpp_09/ex02/jf.cpp
@@ -1,5 +1,9 @@
 #include <vector>
+#include <map>
 #include <iostream>
+#include <chrono>
+#include <ctime>
+#include <algorithm>
 
 //Individual number jenerator for gJ
 int gJN(int n)
@@ -47,37 +51,80 @@ std::vector<int> gFJS(int n_pairs)
 	return (v);
 }
 
-void recMI(std::vector<std::vector<int>> &vec)
-{
-	std::vector<int> leftover;
-
-	if (vec.back()[0] == -1)
-	{
-		leftover = vec.back();
-		vec.pop_back();
-	}
-	for (int i = 0; i < vec.size(); i++)
-	{
-		if (vec[i][0] < vec[i][1])
-			std::swap(vec[i][0], vec[i][1]);
-	}
-}
-
 std::vector<int> merginsertion(std::vector<int> v)
 {
+	if (v.size() == 0 || v.size() == 1)
+		return (v);
+	int leftover = -1;
 	int pairs = v.size() / 2;
-	std::vector<std::vector<int>> vec(pairs + (v.size() % 2));
+	std::map<int, std::vector<int>> wmap;
+	std::vector<int> winners;
 	for (int i = 0; i < pairs; i++)
 	{
-		vec[i][0] = v[(i * 2)];
-		vec[i][1] = v[(i * 2) + 1];
+		if (v[i * 2] < v[(i * 2) + 1])
+			std::swap(v[i * 2], v[(i * 2) + 1]);
+		winners.push_back(v[(i * 2)]);
+		wmap[v[(i * 2)]].push_back(v[(i * 2) + 1]);
 	}
-	if ((pairs * 2) < v.size())
+	if (v.size() > (pairs * 2))
+		leftover = v.back();
+	winners = merginsertion(winners);
+	std::vector<int> gfjs = gFJS(pairs); //geting the ford johnson sequence
+	std::vector<int> sorted = winners; //copying winners so it does not get changed
+	int offset = 0; //after each insertion the offset is incremented just in case.
+	for (int i = 0; i < gfjs.size(); i++)
 	{
-		vec[pairs][0] = -1;
-		vec[pairs][1] = v[v.size() - 1];
+		int winner_index = gfjs[i] - 1;
+		int winner = winners[winner_index];
+		int loser = wmap[winner].back();
+		wmap[winner].pop_back();
+		std::vector<int>::iterator end = sorted.begin() + winner_index + offset;
+		std::vector<int>::iterator pos = std::lower_bound(sorted.begin(), end, loser);
+		sorted.insert(pos, loser);
 	}
-	recMI()
+	if (leftover == -1)
+		return sorted;
+	std::vector<int>::iterator pos = std::lower_bound(sorted.begin(), sorted.end(), leftover);
+	sorted.insert(pos, leftover);
+	return sorted;
+}
+
+
+std::vector<int> merginsertion_find(std::vector<int> v)
+{
+	if (v.size() == 0 || v.size() == 1)
+		return (v);
+	int leftover = -1;
+	int pairs = v.size() / 2;
+	std::map<int, std::vector<int>> wmap;
+	std::vector<int> winners;
+	for (int i = 0; i < pairs; i++)
+	{
+		if (v[i * 2] < v[(i * 2) + 1])
+			std::swap(v[i * 2], v[(i * 2) + 1]);
+		winners.push_back(v[(i * 2)]);
+		wmap[v[(i * 2)]].push_back(v[(i * 2) + 1]);
+	}
+	if (v.size() > (pairs * 2))
+		leftover = v.back();
+	winners = merginsertion(winners);
+	std::vector<int> gfjs = gFJS(pairs); //geting the ford johnson sequence
+	std::vector<int> sorted = winners; //copying winners so it does not get changed
+	for (int i = 0; i < gfjs.size(); i++)
+	{
+		int winner_index = gfjs[i] - 1;
+		int winner = winners[winner_index];
+		int loser = wmap[winner].back();
+		wmap[winner].pop_back();
+		std::vector<int>::iterator end = std::find(sorted.begin(), sorted.end(), winner);
+		std::vector<int>::iterator pos = std::lower_bound(sorted.begin(), end, loser);
+		sorted.insert(pos, loser);
+	}
+	if (leftover == -1)
+		return sorted;
+	std::vector<int>::iterator pos = std::lower_bound(sorted.begin(), sorted.end(), leftover);
+	sorted.insert(pos, leftover);
+	return sorted;
 }
 
 void putvect(std::vector<int> vec)
@@ -93,8 +140,20 @@ void putvect(std::vector<int> vec)
 
 int main(int argc, char *argv[])
 {
-	(void)argc;
-	std::vector<int> v = gFJS(std::stoi(argv[1]));
-	putvect(v);
+	std::vector<int> v;
+	for (int i = 1; i < argc; i++)
+		v.push_back(std::stoi(argv[i]));
+	std::vector<int> v2 = v;
+
+	clock_t starta = clock();
+	v2 = merginsertion(v2);
+	clock_t enda = clock();
+	std::cout << "find method: " << double(enda - starta) << std::endl;
+	//putvect(v);
+
+	clock_t start = clock();
+	v = merginsertion(v);
+	clock_t end = clock();
+	std::cout << "no find method: " << double(end - start) << std::endl;
 }
 
